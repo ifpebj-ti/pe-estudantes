f6b190749b485fa13bd4dcdb284af7faad712b47
Luiz Carlos Oliveira Maciel <luiz.carlos@propig.com.br>
2025-03-24 17:08:01 -0300

    feat: Add swagger and validations in dto's

--- back/src/users/dto/update-user.dto.ts
+++ back/src/users/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @ApiProperty({
+    description: "Nome do usuário",
+    example: "Luizin"
+    })
+    @IsNotEmpty({ message: "O campo name não deve estar vazio." })
+    @IsString({ message: "O campo name deve ser uma string." })
+    name: string;
+}

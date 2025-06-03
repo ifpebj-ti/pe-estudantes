2cff28c1f0df62150b48dd84391f2a2c7511427f
Guilherme Lyare | Grupo Moura <guilherme.lyare@grupomoura.com>
2025-06-02 22:31:58 -0300

    fix: adjusting pages

--- front/src/app/new-password/page.tsx
+++ front/src/app/new-password/page.tsx
@@ -1,97 +1,64 @@
-// "use client";
+"use client";
 
-// import dynamic from "next/dynamic";
-// import { useState } from "react";
-// import Image from "next/image";
-// import "@govbr-ds/core/dist/core.min.css";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import Image from "next/image";
+import React from "react";
+import "@govbr-ds/core/dist/core.min.css";
 
-// // Importações dinâmicas
-// const BrInput = dynamic(() =>
-//   import("@govbr-ds-testing/webcomponents-react").then((mod) => mod.BrInput), { ssr: false }
-// );
+// Importações dinâmicas
+const BrButton = dynamic(() =>
+  import("@govbr-ds-testing/webcomponents-react").then((mod) => mod.BrButton), { ssr: false }
+);
 
-// const BrButton = dynamic(() =>
-//   import("@govbr-ds-testing/webcomponents-react").then((mod) => mod.BrButton), { ssr: false }
-// );
-
-// export default function NewPasswordPage() {
+export default function NewPasswordPage() {
 //   const [formData, setFormData] = useState({
 //     senha: "",
 //     confirmarSenha: ""
 //   });
 
-//   const [mensagem, setMensagem] = useState("");
+  const [mensagem, setMensagem] = useState("");
 
-//   setMensagem("Sua senha foi alterada com sucesso!");
-//   const handleChange = (e: any) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
 
-//   return (
-//     <div className="min-h-screen flex flex-row bg-gradient-to-r from-emerald-100 to-white">
-//       <section className="items-center justify-center min-w-1/2 md:flex hidden">
-//         <Image
-//           src="/login.svg"
-//           alt="Imagem login"
-//           width={600}
-//           height={400}
-//           priority
-//         />
-//       </section>
+  return (
+    <div className="min-h-screen flex flex-row bg-gradient-to-r from-emerald-100 to-white">
+      <section className="items-center justify-center min-w-1/2 md:flex hidden">
+        <Image
+          src="/login.svg"
+          alt="Imagem login"
+          width={600}
+          height={400}
+          priority
+        />
+      </section>
 
-//       <section className="h-screen flex flex-col items-center justify-between md:min-w-1/2 min-w-full py-4">
-//         <div></div>
+      <section className="h-screen flex flex-col items-center justify-between md:min-w-1/2 min-w-full py-4">
+        <div></div>
 
-//         {/* Formulário de nova senha */}
-//         <form className="p-8 w-full max-w-md space-y-6 flex flex-col items-center justify-center">
-//           <h1 className="text-2xl font-bold text-center">Nova Senha</h1>
+        {/* Formulário de nova senha */}
+        <form className="p-8 w-full max-w-md space-y-6 flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold text-center">Nova Senha</h1>
 
-//           <BrInput
-//             label="Senha"
-//             type="password"
-//             name="senha"
-//             required
-//             pattern=".{8,}"
-//             icon
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//             <Image width={15} height={10} slot="icon" src="/locker.svg" alt="Ícone senha" />
-//           </BrInput>
+          <BrButton
+            type="submit"
+            active
+            block
+            class="w-full"
+          >
+            Finalizar
+          </BrButton>
 
-//           <BrInput
-//             label="Confirmar Senha"
-//             type="password"
-//             name="confirmarSenha"
-//             required
-//             pattern=".{8,}"
-//             icon
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//             <Image width={15} height={10} slot="icon" src="/locker.svg" alt="Ícone confirmar senha" />
-//           </BrInput>
+          {mensagem && (
+            <p className="text-center text-sm text-green-700">{mensagem}</p>
+          )}
+        </form>
 
-//           <BrButton
-//             type="submit"
-//             active
-//             block
-//             class="w-full"
-//           >
-//             Finalizar
-//           </BrButton>
-
-//           {mensagem && (
-//             <p className="text-center text-sm text-green-700">{mensagem}</p>
-//           )}
-//         </form>
-
-//         <div className="flex items-center justify-center w-full">
-//           <a className="text-sm" href="/forgot-password">
-//             Precisa de Ajuda?
-//           </a>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
+        <div className="flex items-center justify-center w-full" onClick={() => setMensagem("Senha alterada com sucesso!")}>
+          <a className="text-sm" href="/forgot-password">
+            Precisa de Ajuda?
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+}
--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -1,20 +1,17 @@
-// "use client";
+"use client";
 
-// import dynamic from "next/dynamic";
-// import Image from "next/image";
-// import "@govbr-ds/core/dist/core.min.css";
-// import { useState } from "react";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import "@govbr-ds/core/dist/core.min.css";
+import React from "react";
 
-// // Importação dinâmica dos componentes BR
-// const BrInput = dynamic(() =>
-//   import("@govbr-ds-testing/webcomponents-react").then((mod) => mod.BrInput), { ssr: false }
-// );
+// Importação dinâmica dos componentes BR
 
-// const BrButton = dynamic(() =>
-//   import("@govbr-ds-testing/webcomponents-react").then((mod) => mod.BrButton), { ssr: false }
-// );
+const BrButton = dynamic(() =>
+  import("@govbr-ds-testing/webcomponents-react").then((mod) => mod.BrButton), { ssr: false }
+);
 
-// export default function RegisterPage() {
+export default function RegisterPage() {
 //   const [formData, setFormData] = useState({
 //     nome: "",
 //     email: "",
@@ -23,105 +20,47 @@
 //     confirmarSenha: ""
 //   });
 
-//   const handleChange = (e: any) => {
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 //     setFormData({ ...formData, [e.target.name]: e.target.value });
 //   };
 
-//   return (
-//     <div className="min-h-screen flex flex-row bg-gradient-to-r from-emerald-100 to-white">
-//       <section className="items-center justify-center min-w-1/2 md:flex hidden">
-//         <Image
-//           src="/login.svg"
-//           alt="Imagem login"
-//           width={600}
-//           height={400}
-//           priority
-//         />
-//       </section>
+  return (
+    <div className="min-h-screen flex flex-row bg-gradient-to-r from-emerald-100 to-white">
+      <section className="items-center justify-center min-w-1/2 md:flex hidden">
+        <Image
+          src="/login.svg"
+          alt="Imagem login"
+          width={600}
+          height={400}
+          priority
+        />
+      </section>
 
-//       <section className="h-screen flex flex-col items-center justify-between md:min-w-1/2 min-w-full py-4">
-//         <div></div>
+      <section className="h-screen flex flex-col items-center justify-between md:min-w-1/2 min-w-full py-4">
+        <div></div>
 
-//         {/* Formulário de cadastro */}
-//         <form className="p-8 w-full max-w-md space-y-6 flex flex-col items-center justify-center">
-//           <h1 className="text-2xl font-bold text-center">Cadastro</h1>
+        {/* Formulário de cadastro */}
+        <form className="p-8 w-full max-w-md space-y-6 flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold text-center">Cadastro</h1>
 
-//           <BrInput
-//             label="Nome completo"
-//             name="nome"
-//             required
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//             <Image width={15} height={10} slot="icon" src="/user.svg" alt="Ícone nome" />
-//           </BrInput>
+          
 
-//           <BrInput
-//             label="Email"
-//             type="email"
-//             name="email"
-//             required
-//             pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-//             icon
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//             <Image width={15} height={10} slot="icon" src="/user.svg" alt="Ícone email" />
-//           </BrInput>
+          <BrButton
+            type="submit"
+            active
+            block
+            class="w-full"
+          >
+            Finalizar
+          </BrButton>
+        </form>
 
-//           <BrInput
-//             label="CPF"
-//             name="cpf"
-//             required
-//             pattern="[0-9]{11}"
-//             icon
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//           </BrInput>
-
-//           <BrInput
-//             label="Senha"
-//             type="password"
-//             name="senha"
-//             required
-//             pattern=".{8,}"
-//             icon
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//             <Image width={15} height={10} slot="icon" src="/locker.svg" alt="Ícone senha" />
-//           </BrInput>
-
-//           <BrInput
-//             label="Confirmar Senha"
-//             type="password"
-//             name="confirmarSenha"
-//             required
-//             pattern=".{8,}"
-//             icon
-//             class="w-full"
-//             onInput={handleChange}
-//           >
-//             <Image width={15} height={10} slot="icon" src="/locker.svg" alt="Ícone confirmar senha" />
-//           </BrInput>
-
-//           <BrButton
-//             type="submit"
-//             active
-//             block
-//             class="w-full"
-//           >
-//             Finalizar
-//           </BrButton>
-//         </form>
-
-//         <div className="flex items-center justify-center w-full">
-//           <a className="text-sm" href="/forgot-password">
-//             Precisa de Ajuda?
-//           </a>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
+        <div className="flex items-center justify-center w-full">
+          <a className="text-sm" href="/forgot-password">
+            Precisa de Ajuda?
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+}
